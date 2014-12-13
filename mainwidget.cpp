7817cdfa298437bd75fa98dc2b57e6d37ef572a3
Shinnok <admin@shinnok.com>
2014-12-13 15:23:12 +0200

    Improvements.

--- mainwidget.cpp
+++ mainwidget.cpp
@@ -6,6 +6,12 @@ MainWidget::MainWidget(QWidget *parent) :
     ui(new Ui::MainWidget)
 {
     ui->setupUi(this);
+
+    QLabel *tarsnapLogo = new QLabel(this);
+    tarsnapLogo->setText("Tarsnap");
+    tarsnapLogo->setStyleSheet("font: italic \"Lucida\"; font-size: 22pt ;");
+    tarsnapLogo->move(this->width()-tarsnapLogo->width()-10,0);
+    tarsnapLogo->show();
 }
 
 MainWidget::~MainWidget()

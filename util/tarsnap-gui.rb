68c19bca233a46a5fd27c4152b1c9cef6809a964
Shinnok <admin@shinnok.com>
2016-05-06 17:28:04 -0400

    Update Homebrew recipe to v0.9.
    Also add macdeployqt step so that the resulting .app bundle is self contained.

--- util/tarsnap-gui.rb
+++ util/tarsnap-gui.rb
@@ -1,8 +1,8 @@
 class TarsnapGui < Formula
   desc "Cross platform GUI for the Tarsnap command-line client"
   homepage "https://github.com/Tarsnap/tarsnap-gui/wiki/Tarsnap"
-  url "https://github.com/Tarsnap/tarsnap-gui/archive/v0.8.tar.gz"
-  sha256 "a4b5b23b6b75aa7a4af4d124521286c80d42446571cb27a858d03e772b2ff080"
+  url "https://github.com/Tarsnap/tarsnap-gui/archive/v0.9.tar.gz"
+  sha256 "e47e1e611f2c7bb5111bcc1d2d86fa2c045ba4af23f8430bbc3c72f546572cb8"
   head "https://github.com/Tarsnap/tarsnap-gui.git"
 
   depends_on "qt5"
@@ -11,6 +11,7 @@ class TarsnapGui < Formula
   def install
     system "qmake"
     system "make"
+    system "macdeployqt Tarsnap.app"
     prefix.install "Tarsnap.app"
   end
 

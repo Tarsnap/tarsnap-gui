9d0e577596ea34861f272702387fc804c57d795c
Graham Percival <gperciva@tarsnap.com>
2016-09-20 13:36:53 -0700

    ArchiveListWidgetItem: doxygen

--- src/widgets/archivelistwidgetitem.h
+++ src/widgets/archivelistwidgetitem.h
@@ -7,27 +7,44 @@
 #include <QListWidgetItem>
 #include <QObject>
 
+/*!
+ * \ingroup widgets-specialized
+ * \brief The ArchiveListWidgetItem is a QListWidgetItem which
+ * displays information about an archive.
+ */
 class ArchiveListWidgetItem : public QObject, public QListWidgetItem
 {
     Q_OBJECT
 
 public:
+    //! Constructor.
+    //! \param archive: pointer to the archive to display.
     explicit ArchiveListWidgetItem(ArchivePtr archive);
     ~ArchiveListWidgetItem();
 
+    //! Returns a widget with a graphical representation of this archive.
     QWidget *widget();
 
+    //! Returns a pointer to the archive.
     ArchivePtr archive() const;
+
+    //! Indicate that this archive is scheduled for deletion.
     void setDisabled();
+    //! Returns whether this archive is scheduled for deletion.
     bool isDisabled();
 
 signals:
+    //! The user requested to remove this item from the archive list.
     void requestDelete();
+    //! The user requested more information about this archive.
     void requestInspect();
+    //! The user requested to restore this archive.
     void requestRestore();
+    //! The user requested to see the Job which created this archive.
     void requestGoToJob();
 
 protected:
+    //! Processes requests to switch language.
     bool eventFilter(QObject *obj, QEvent *event);
 
 private slots:

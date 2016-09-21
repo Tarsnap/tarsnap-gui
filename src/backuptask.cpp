e8407ef8ef758e315e5a712f8ed09405ffbf1dec
Shinnok <admin@shinnok.com>
2016-09-21 11:56:19 +0300

    Init SkipNoDump option with global settings value.

--- src/backuptask.cpp
+++ src/backuptask.cpp
@@ -25,6 +25,7 @@ BackupTask::BackupTask()
         settings.value("tarsnap/traverse_mount", true).toBool());
     setOptionFollowSymLinks(
         settings.value("tarsnap/follow_symlinks", false).toBool());
+    setOptionSkipNoDump(settings.value("app/skip_nodump", false).toBool());
     setOptionSkipFilesSize(settings.value("app/skip_files_size", 0).toInt());
     setOptionSkipSystem(settings.value("app/skip_system_enabled", false).toBool());
     setOptionSkipSystemFiles(

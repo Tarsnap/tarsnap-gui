a739a616041ff76eaf65c6fa7de84ed3ac998a6f
Graham Percival <gperciva@tarsnap.com>
2017-06-10 09:40:40 -0700

    Doxygen comments for Notification

--- src/notification.h
+++ src/notification.h
@@ -4,16 +4,23 @@
 #include <QObject>
 #include <QSystemTrayIcon>
 
+/*!
+ * \ingroup misc
+ * \brief The Notification widget is a QSystemTrayIcon which displays
+ * a "pop-up" notification in the user's main desktop.
+ */
 class Notification : public QSystemTrayIcon
 {
     Q_OBJECT
+
 public:
+    //! Constructor.
     explicit Notification(QSystemTrayIcon *parent = nullptr);
 
 public slots:
+    //! Displays a string if the user has "Show desktop notifications" enabled,
+    //! after removing HTML tags.
     void displayNotification(QString message);
-
-signals:
 };
 
 #endif // NOTIFICATION_H

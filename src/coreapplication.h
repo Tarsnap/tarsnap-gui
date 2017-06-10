2e62739436d375c4d5dc0402d02224221e449000
Graham Percival <gperciva@tarsnap.com>
2017-06-10 10:32:22 -0700

    Doxygen comments for CoreApplication

--- src/coreapplication.h
+++ src/coreapplication.h
@@ -10,19 +10,32 @@
 #include <QObject>
 #include <QSettings>
 
+/*!
+ * \ingroup misc
+ * \brief the CoreApplication is a QApplication which launches the MainWindow,
+ * SetupWindow (if applicable), and manages background tasks.
+ */
 class CoreApplication : public QApplication
 {
     Q_OBJECT
 
 public:
+    //! Constructor.
     CoreApplication(int &argc, char **argv);
     ~CoreApplication();
 
+    //! Parses command-line args.
     void parseArgs();
+    //! Initializes the QSettings, Translator, PersistentStore, and launches
+    //! the Setup wizard (if necessary).
     bool initialize();
 
 public slots:
+    //! Removes the MainWindow (if it exists), resets the PersistentStore
+    //! and app Settings, then runs initialize().  Used as a preliminary
+    //! step before the SetupWizard.
     bool reinit();
+    //! Creates a MainWindow.
     void showMainWindow();
 
 private:

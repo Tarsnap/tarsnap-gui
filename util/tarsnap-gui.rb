0f44d4b490c7d30cafc689cb479c2c7f912f4245
Shinnok <admin@shinnok.com>
2017-07-27 19:02:14 +0200

    Update Homebrew recipe shasum for v1.0 tarball.

--- util/tarsnap-gui.rb
+++ util/tarsnap-gui.rb
@@ -2,7 +2,7 @@ class TarsnapGui < Formula
   desc "Cross-platform GUI front-end for the Tarsnap command-line client."
   homepage "https://github.com/Tarsnap/tarsnap-gui/wiki/Tarsnap"
   url "https://github.com/Tarsnap/tarsnap-gui/archive/v1.0.tar.gz"
-  sha256 ""
+  sha256 "cd21d2a85f073e72f10900632fdcb49956985255a5711fb4f6d434433b09dac9"
   head "https://github.com/Tarsnap/tarsnap-gui.git"
 
   depends_on "qt5"

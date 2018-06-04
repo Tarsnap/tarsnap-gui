879a05df3a9dd908e71570da2e4420b1df6a13d1
Graham Percival <gperciva@tarsnap.com>
2018-06-04 15:06:03 -0700

    release: update Homebrew file for 1.0.1

--- util/tarsnap-gui.rb
+++ util/tarsnap-gui.rb
@@ -1,8 +1,8 @@
 class TarsnapGui < Formula
   desc "Cross-platform GUI front-end for the Tarsnap command-line client."
   homepage "https://github.com/Tarsnap/tarsnap-gui/wiki/Tarsnap"
-  url "https://github.com/Tarsnap/tarsnap-gui/archive/v1.0.tar.gz"
-  sha256 "cd21d2a85f073e72f10900632fdcb49956985255a5711fb4f6d434433b09dac9"
+  url "https://github.com/Tarsnap/tarsnap-gui/archive/v1.0.1.tar.gz"
+  sha256 "502e53a4eacb2795bfbe62b9e6ef236cce6fdb43cb15153c0fc4ad61c161eb97"
   head "https://github.com/Tarsnap/tarsnap-gui.git"
 
   depends_on "qt"

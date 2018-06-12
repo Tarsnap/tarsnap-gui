8eab15fcc6b38a0ee0e350499800ea92da8d91c3
Graham Percival <gperciva@tarsnap.com>
2018-06-12 14:37:11 -0700

    tests/mainwindow: show visually if GUI

--- tests/mainwindow/main.cpp
+++ tests/mainwindow/main.cpp
@@ -1,6 +1,15 @@
 #include <QtTest/QtTest>
 
+#include "../qtest-platform.h"
+
 #include "mainwindow.h"
+
+#define VISUAL_INIT                                                            \
+    IF_VISUAL                                                                  \
+    {                                                                          \
+        mainwindow->show();                                                    \
+        VISUAL_WAIT;                                                           \
+    }
 
 class TestMainWindow : public QObject
 {
@@ -48,29 +57,37 @@ void TestMainWindow::about_window()
     MainWindow *   mainwindow = new MainWindow();
     Ui::MainWindow ui         = mainwindow->_ui;
 
+    VISUAL_INIT;
+
     // Starts off not visible and the button is not pushed down
+    ui.actionGoHelp->trigger();
     QVERIFY(mainwindow->_aboutWindow.isVisible() == false);
     QVERIFY(ui.aboutButton->isChecked() == false);
+    VISUAL_WAIT;
 
     // Becomes visible
     ui.aboutButton->click();
     QVERIFY(mainwindow->_aboutWindow.isVisible() == true);
     QVERIFY(ui.aboutButton->isChecked() == true);
+    VISUAL_WAIT;
 
     // Becomes invisible by clicking the button again
     ui.aboutButton->click();
     QVERIFY(mainwindow->_aboutWindow.isVisible() == false);
     QVERIFY(ui.aboutButton->isChecked() == false);
+    VISUAL_WAIT;
 
     // Becomes visible
     ui.aboutButton->click();
     QVERIFY(mainwindow->_aboutWindow.isVisible() == true);
     QVERIFY(ui.aboutButton->isChecked() == true);
+    VISUAL_WAIT;
 
     // Becomes invisible by closing the About window
     mainwindow->_aboutWindow.close();
     QVERIFY(mainwindow->_aboutWindow.isVisible() == false);
     QVERIFY(ui.aboutButton->isChecked() == false);
+    VISUAL_WAIT;
 
     // Test the "About Tarsnap" menubar item, if applicable.
     if(mainwindow->_menuBar != nullptr)
@@ -78,21 +95,25 @@ void TestMainWindow::about_window()
         // find "About Tarsnap" menu item
         QAction *menuAction = get_menubar_about(mainwindow->_menuBar);
         QVERIFY(menuAction != nullptr);
+        VISUAL_WAIT;
 
         // Becomes visible using the menu bar action
         menuAction->trigger();
         QVERIFY(mainwindow->_aboutWindow.isVisible() == true);
         QVERIFY(ui.aboutButton->isChecked() == true);
+        VISUAL_WAIT;
 
         // Stay visible even when clicking the menu bar action again
         menuAction->trigger();
         QVERIFY(mainwindow->_aboutWindow.isVisible() == true);
         QVERIFY(ui.aboutButton->isChecked() == true);
+        VISUAL_WAIT;
 
         // Becomes invisible by clicking the Help->About button
         ui.aboutButton->click();
         QVERIFY(mainwindow->_aboutWindow.isVisible() == false);
         QVERIFY(ui.aboutButton->isChecked() == false);
+        VISUAL_WAIT;
     }
 
     delete mainwindow;

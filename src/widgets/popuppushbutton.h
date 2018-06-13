fc0c38cedf2cbc1928266d7add1d423209e28b4e
Graham Percival <gperciva@tarsnap.com>
2018-06-13 15:35:41 -0700

    PopupPushButton: add widget

---
+++ src/widgets/popuppushbutton.h
@@ -0,0 +1,32 @@
+#ifndef POPUPPUSHBUTTON_H
+#define POPUPPUSHBUTTON_H
+
+#include <QDialog>
+#include <QPushButton>
+
+/*!
+ * \ingroup widgets-general
+ * \brief The PopupPushButton widget is a QPushButton which shows a non-modal
+ * QDialog in a pop-up window.
+ */
+class PopupPushButton : public QPushButton
+{
+    Q_OBJECT
+
+public:
+    //! Constructor.
+    explicit PopupPushButton(QWidget *parent = nullptr);
+    ~PopupPushButton();
+
+    //! Display popup if and only if this button is checked.
+    void setPopup(QDialog *popup);
+
+private slots:
+    void valueChanged(bool checked);
+    void popupClosed(int result);
+
+private:
+    QDialog *_popup;
+};
+
+#endif // POPUPPUSHBUTTON_H

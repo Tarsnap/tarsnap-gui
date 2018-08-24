0f4d6950de7779c8fb61c8178c2492b29d85f748
Graham Percival <graham@percival-music.ca>
2018-08-23 17:14:24 -0700

    findTarsnapClientInPath(): also search /usr/local/bin
    MacOS X's brew puts binaries in /usr/local/bin but that's not in the $PATH for
    GUI apps:
      https://docs.brew.sh/FAQ#my-mac-apps-dont-find-usrlocalbin-utilities
    
    In the interest of making it easy for people to get running with Tarsnap, we'll
    hard-code searching that directory as well.

--- src/utils.cpp
+++ src/utils.cpp
@@ -114,6 +114,13 @@ QString Utils::findTarsnapClientInPath(QString path, bool keygenToo)
         searchPaths << path;
 
     executable = QStandardPaths::findExecutable(CMD_TARSNAP, searchPaths);
+#if defined(Q_OS_OSX)
+    // If we haven't found the command in the default PATH, look
+    // in /usr/local/bin because that's where brew puts it.
+    QStringList brew_bin = {"/usr/local/bin"};
+    if(executable.isEmpty() && searchPaths.isEmpty())
+        executable = QStandardPaths::findExecutable(CMD_TARSNAP, brew_bin);
+#endif
     if(executable.isEmpty() || !QFileInfo(executable).isReadable()
        || !QFileInfo(executable).isExecutable())
         return "";
@@ -121,6 +128,11 @@ QString Utils::findTarsnapClientInPath(QString path, bool keygenToo)
     {
         executable =
             QStandardPaths::findExecutable(CMD_TARSNAPKEYGEN, searchPaths);
+#if defined(Q_OS_OSX)
+        if(executable.isEmpty() && searchPaths.isEmpty())
+            executable =
+                QStandardPaths::findExecutable(CMD_TARSNAPKEYGEN, brew_bin);
+#endif
     }
 
     if(executable.isEmpty() || !QFileInfo(executable).isReadable()

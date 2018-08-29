49c5bf34c889a3ccf6858205006d4c599ba11819
Graham Percival <gperciva@tarsnap.com>
2018-08-28 22:49:33 -0700

    ConsoleLog: remove LOG messages going to console
    I should probably rename ConsoleLog to Log, but that can happen in a later
    commit.

--- src/debug.h
+++ src/debug.h
@@ -11,12 +11,6 @@
 #define DEBUG qDebug()
 #endif
 
-#if(QT_VERSION >= QT_VERSION_CHECK(5, 4, 0))
-#define WARN qWarning().noquote()
-#else
-#define WARN qWarning()
-#endif
-
 #define LOG ConsoleLog::instance()
 #define DEFAULT_LOG_FILE "tarsnap.log"
 
@@ -40,126 +34,108 @@ public:
 
     inline ConsoleLog &operator<<(QChar t)
     {
-        WARN << t;
         emit message(QString(t));
         saveLogMessage(QString(t));
         return *this;
     }
     inline ConsoleLog &operator<<(bool t)
     {
-        WARN << t;
         emit message(QString(t));
         saveLogMessage(QString(t));
         return *this;
     }
     inline ConsoleLog &operator<<(char t)
     {
-        WARN << t;
         emit message(QString(t));
         saveLogMessage(QString(t));
         return *this;
     }
     inline ConsoleLog &operator<<(signed short t)
     {
-        WARN << t;
         emit message(QString::number(t));
         saveLogMessage(QString::number(t));
         return *this;
     }
     inline ConsoleLog &operator<<(unsigned short t)
     {
-        WARN << t;
         emit message(QString::number(t));
         saveLogMessage(QString::number(t));
         return *this;
     }
     inline ConsoleLog &operator<<(signed int t)
     {
-        WARN << t;
         emit message(QString::number(t));
         saveLogMessage(QString::number(t));
         return *this;
     }
     inline ConsoleLog &operator<<(unsigned int t)
     {
-        WARN << t;
         emit message(QString::number(t));
         saveLogMessage(QString::number(t));
         return *this;
     }
     inline ConsoleLog &operator<<(signed long t)
     {
-        WARN << t;
         emit message(QString::number(t));
         saveLogMessage(QString::number(t));
         return *this;
     }
     inline ConsoleLog &operator<<(unsigned long t)
     {
-        WARN << t;
         emit message(QString::number(t));
         saveLogMessage(QString::number(t));
         return *this;
     }
     inline ConsoleLog &operator<<(qint64 t)
     {
-        WARN << t;
         emit message(QString::number(t));
         saveLogMessage(QString::number(t));
         return *this;
     }
     inline ConsoleLog &operator<<(quint64 t)
     {
-        WARN << t;
         emit message(QString::number(t));
         saveLogMessage(QString::number(t));
         return *this;
     }
     inline ConsoleLog &operator<<(float t)
     {
-        WARN << t;
         emit message(QString::number(t));
         saveLogMessage(QString::number(t));
         return *this;
     }
     inline ConsoleLog &operator<<(double t)
     {
-        WARN << t;
         emit message(QString::number(t));
         saveLogMessage(QString::number(t));
         return *this;
     }
     inline ConsoleLog &operator<<(const char *t)
     {
-        WARN << t;
         emit message(QString::fromUtf8(t));
         saveLogMessage(QString::fromUtf8(t));
         return *this;
     }
     inline ConsoleLog &operator<<(const QString &t)
     {
-        WARN << t;
         emit message(t);
         saveLogMessage(t);
         return *this;
     }
     inline ConsoleLog &operator<<(const QStringRef &t)
     {
-        WARN << t;
         emit message(t.toString());
         saveLogMessage(t.toString());
         return *this;
     }
     inline ConsoleLog &operator<<(QLatin1String t)
     {
-        WARN << t;
         emit message(QString(t));
         saveLogMessage(QString(t));
         return *this;
     }
     inline ConsoleLog &operator<<(const QByteArray &t)
     {
-        WARN << t;
         emit message(QString(t));
         saveLogMessage(QString(t));
         return *this;

48a2972c2d76b985d5ba14af84162c8b5ebf66d6
Graham Percival <gperciva@tarsnap.com>
2018-10-09 16:47:21 -0700

    tests/jobstabwidget: test DefaultJobs

--- tests/jobstabwidget/test-jobstabwidget.cpp
+++ tests/jobstabwidget/test-jobstabwidget.cpp
@@ -13,6 +13,9 @@ class TestJobsTabWidget : public QObject
 private slots:
     void initTestCase();
     void cleanupTestCase();
+
+    // This one needs to be first
+    void defaultJobs();
 };
 
 void TestJobsTabWidget::initTestCase()
@@ -29,6 +32,28 @@ void TestJobsTabWidget::initTestCase()
 void TestJobsTabWidget::cleanupTestCase()
 {
     TSettings::destroy();
+}
+
+void TestJobsTabWidget::defaultJobs()
+{
+    JobsTabWidget *   jobstabwidget = new JobsTabWidget();
+    Ui::JobsTabWidget ui            = jobstabwidget->_ui;
+    QSignalSpy        sig_jobAdded(jobstabwidget,
+                            SIGNAL(temp_jobDetailsWidget_jobAdded(JobPtr)));
+
+    VISUAL_INIT(jobstabwidget);
+    jobstabwidget->show();
+
+    // We start off showing the default jobs prompt
+    QVERIFY(ui.defaultJobs->isVisible() == true);
+
+    // Don't create the default jobs; no more prompt
+    ui.dismissButton->clicked();
+    QVERIFY(ui.defaultJobs->isVisible() == false);
+    QVERIFY(sig_jobAdded.count() == 0);
+    VISUAL_WAIT;
+
+    delete jobstabwidget;
 }
 
 QTEST_MAIN(TestJobsTabWidget)

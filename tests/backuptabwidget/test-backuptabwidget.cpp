d94ddfd1b88799fd5d63168c41c41a40b29734c0
Graham Percival <gperciva@tarsnap.com>
2018-10-12 14:52:46 -0700

    tests/backuptabwidget: test backupNameLineEdit

--- tests/backuptabwidget/test-backuptabwidget.cpp
+++ tests/backuptabwidget/test-backuptabwidget.cpp
@@ -10,6 +10,8 @@ class TestBackupTabWidget : public QObject
 
 private slots:
     void initTestCase();
+
+    void nameEdit();
 };
 
 void TestBackupTabWidget::initTestCase()
@@ -21,6 +23,30 @@ void TestBackupTabWidget::initTestCase()
         // Use a custom message handler to filter out unwanted messages
         orig_message_handler = qInstallMessageHandler(offscreenMessageOutput);
     }
+}
+
+void TestBackupTabWidget::nameEdit()
+{
+    BackupTabWidget *   backuptabwidget = new BackupTabWidget();
+    Ui::BackupTabWidget ui              = backuptabwidget->_ui;
+
+    VISUAL_INIT(backuptabwidget);
+
+    // Start off without anything in the LineEdit
+    QVERIFY(ui.backupNameLineEdit->text().isEmpty());
+    VISUAL_WAIT;
+
+    // Append the timestamp
+    ui.appendTimestampCheckBox->toggle();
+    QVERIFY(ui.backupNameLineEdit->text().isEmpty() == false);
+    VISUAL_WAIT;
+
+    // Remove the timestamp
+    ui.appendTimestampCheckBox->toggle();
+    QVERIFY(ui.backupNameLineEdit->text().isEmpty());
+    VISUAL_WAIT;
+
+    delete backuptabwidget;
 }
 
 QTEST_MAIN(TestBackupTabWidget)

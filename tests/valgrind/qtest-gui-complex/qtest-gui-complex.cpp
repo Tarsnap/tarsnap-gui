ff686b8206ec0a5e4ab55fc3505b6ae725ea5a38
Graham Percival <gperciva@tarsnap.com>
2018-11-21 21:29:45 -0800

    tests/valgrind: suppressions for customfilesystemmodel

--- tests/valgrind/qtest-gui-complex/qtest-gui-complex.cpp
+++ tests/valgrind/qtest-gui-complex/qtest-gui-complex.cpp
@@ -1,5 +1,6 @@
 #include <QtTest/QtTest>
 
+#include <QFileSystemModel>
 #include <QFontMetrics>
 
 class TestQTestComplex : public QObject
@@ -9,6 +10,8 @@ class TestQTestComplex : public QObject
 private slots:
     void pl_nothing();
     void pl_fontmetric_elidedText();
+    void pl_processEvents();
+    void pl_filesystemmodel();
 };
 
 void TestQTestComplex::pl_nothing()
@@ -20,6 +23,22 @@ void TestQTestComplex::pl_fontmetric_elidedText()
     QFontMetrics metrics(qApp->font());
     QString text = metrics.elidedText("this is a long long long piece of text",
                                       Qt::ElideRight, 100);
+}
+
+void TestQTestComplex::pl_processEvents()
+{
+    QCoreApplication::processEvents(0, 100);
+}
+
+void TestQTestComplex::pl_filesystemmodel()
+{
+    QFileSystemModel *model = new QFileSystemModel();
+
+    // Start to read the path (this starts a new thread)
+    model->setRootPath(QDir::currentPath());
+    QCoreApplication::processEvents(0, 100);
+
+    delete model;
 }
 
 QTEST_MAIN(TestQTestComplex)

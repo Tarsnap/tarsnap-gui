ef8b2589d2693b667dbb5c9fdbc31f2527f9429a
Graham Percival <gperciva@tarsnap.com>
2019-01-04 10:41:34 -0800

    tests: add run-valgrind-tests.sh

---
+++ tests/run-valgrind-tests.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+set -e
+
+# Command-line
+VALGRINDS="cli consolelog persistent task taskmanager"
+
+# Gui
+VALGRINDS="${VALGRINDS} small-widgets customfilesystemmodel setupwizard"
+
+for D in $VALGRINDS; do
+	cd $D
+	qmake > /dev/null
+	nice make -j3 > /dev/null
+	make test_valgrind
+	cd ..
+done

feeb9cdbd4bb520a3a5959a8dbb14b6bf0734a71
Graham Percival <gperciva@tarsnap.com>
2019-01-29 10:57:25 -0800

    tests/valgrind: cover more UI memleaks

--- tests/valgrind/qtest-gui-ui/ui.cpp
+++ tests/valgrind/qtest-gui-ui/ui.cpp
@@ -20,7 +20,14 @@ void TestUi::pl_filepickerwidget()
 {
     QWidget *             wid = new QWidget();
     Ui::FilePickerWidget *ui  = new Ui::FilePickerWidget;
+
+    // main source of memleaks
     ui->setupUi(wid);
+    // retranslateUi() is called from setupUi(), but calling it again produces
+    // another memleak.  Our main code does call it as part of the translation
+    // layer, so we need to do so here.
+    ui->retranslateUi(wid);
+
     delete ui;
     delete wid;
 }

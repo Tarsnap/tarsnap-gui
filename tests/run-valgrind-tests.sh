098039e77e88e9636d4ac229daf999f25cc01e69
Graham Percival <gperciva@tarsnap.com>
2019-02-28 19:08:39 -0800

    tests/valgrind: split up compiling & testing

--- tests/run-valgrind-tests.sh
+++ tests/run-valgrind-tests.sh
@@ -10,10 +10,17 @@ VALGRINDS="${VALGRINDS} customfilesystemmodel small-widgets setupwizard"
 VALGRINDS="${VALGRINDS} backuptabwidget settingswidget jobstabwidget"
 VALGRINDS="${VALGRINDS} archivestabwidget"
 
+# Build first
 for D in $VALGRINDS; do
 	cd $D
 	qmake > /dev/null
 	nice make -j3 > /dev/null
+	cd ..
+done
+
+# Run tests
+for D in $VALGRINDS; do
+	cd $D
 	make test_valgrind
 	cd ..
 done

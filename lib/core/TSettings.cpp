27ac739aba43c76e74006004bd0ca00c0aec7118
Graham Percival <gperciva@tarsnap.com>
2019-03-13 17:36:14 -0700

    lib/core/TSettings: stylistic improvements

--- lib/core/TSettings.cpp
+++ lib/core/TSettings.cpp
@@ -3,95 +3,88 @@
 WARNINGS_DISABLE
 #include <QFileInfo>
 #include <QMutex>
+#include <QMutexLocker>
 #include <QSettings>
 
 // For safety
 static QMutex mutex;
 WARNINGS_ENABLE
 
-// Filename to use (if we don't want the default).
-static QString filename_next = "";
-
-// The actual settings.
-static QSettings *settings = nullptr;
+// Initialize static members
+QSettings *TSettings::_settings      = nullptr;
+QString *  TSettings::_filename_next = nullptr;
 
 void TSettings::setFilename(QString filename)
 {
-    filename_next = filename;
+    Q_ASSERT(_filename_next == nullptr);
+
+    _filename_next = new QString(filename);
 }
 
 void TSettings::destroy()
 {
     // Bail if we've already cleaned up, including after a mutex (in case a
     // different thread called this function already).
-    if(settings == nullptr)
+    if(_settings == nullptr)
         return;
-    mutex.lock();
-    if(settings == nullptr)
-    {
-        mutex.unlock();
+    QMutexLocker locker(&mutex);
+    if(_settings == nullptr)
         return;
-    }
 
-    // Clean up, release mutex.
-    delete settings;
-    settings      = nullptr;
-    filename_next = "";
-    mutex.unlock();
+    // Clean up.
+    delete _filename_next;
+    delete _settings;
+    _settings      = nullptr;
+    _filename_next = nullptr;
 }
 
 TSettings::TSettings()
 {
     // Bail if we already have an object, including after a mutex (in case a
     // different thread created this object already).
-    if(settings != nullptr)
+    if(_settings != nullptr)
         return;
-    mutex.lock();
-    if(settings != nullptr)
-    {
-        mutex.unlock();
+    QMutexLocker locker(&mutex);
+    if(_settings != nullptr)
         return;
-    }
 
     // If we don't have a custom filename, get the default one for
     // IniFormat, but replace the suffix with ".conf".
-    if(filename_next == "")
+    if(_filename_next == nullptr)
     {
         QSettings::setDefaultFormat(QSettings::IniFormat);
-        QSettings settings_default;
-        QFileInfo name(settings_default.fileName());
+        QSettings _settings_default;
+        QFileInfo name(_settings_default.fileName());
         // Yes, Qt takes care of "/" for non-Unix platforms.
-        filename_next = name.path() + "/" + name.completeBaseName() + ".conf";
+        QString path   = name.path() + "/" + name.completeBaseName() + ".conf";
+        _filename_next = new QString(path);
     }
 
     // Create the QSettings.
-    settings = new QSettings(filename_next, QSettings::IniFormat);
-
-    // Clean up.
-    mutex.unlock();
+    _settings = new QSettings(*_filename_next, QSettings::IniFormat);
 }
 
 QSettings *TSettings::getQSettings()
 {
-    return settings;
+    return _settings;
 }
 
 bool TSettings::contains(const QString &key)
 {
-    return (settings->contains(key));
+    return (_settings->contains(key));
 }
 
 QVariant TSettings::value(const QString &key, const QVariant &defaultValue)
 {
-    return (settings->value(key, defaultValue));
+    return (_settings->value(key, defaultValue));
 }
 
 void TSettings::setValue(const QString &key, const QVariant &value)
 {
-    settings->setValue(key, value);
+    _settings->setValue(key, value);
 }
 
 void TSettings::sync()
 {
-    settings->sync();
+    _settings->sync();
 }

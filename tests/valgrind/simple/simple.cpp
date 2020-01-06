0fa8e8c5eca1e252fc5a7811fbec6db382cf3302
Graham Percival <graham@percival-music.ca>
2020-01-05 21:36:33 -0800

    valgrind: add suppression for QTextCodec

--- tests/valgrind/simple/simple.cpp
+++ tests/valgrind/simple/simple.cpp
@@ -4,12 +4,26 @@ WARNINGS_DISABLE
 #include <QCoreApplication>
 #include <QEventLoop>
 #include <QSqlDatabase>
+#include <QTextCodec>
 WARNINGS_ENABLE
 
 #include "../macros-valgrind.h"
 
 static void pl_nothing()
 {
+}
+
+static void pl_no_app_codec()
+{
+    // To trigger this leak, do *not* use T_APP_BEGIN_CONSOLE.
+    // This is arguably an unfair case, since a lot of Qt is built on the
+    // assumption that there exists exactly 1 QCoreApplication or QApplication.
+    // However, there's no such listed dependency in the docs for QTextCodec,
+    // and this leak occurs whenever you print anything to the console.
+
+    QTextCodec::codecForLocale();
+
+    // To trigger this leak, do *not* use T_APP_END.
 }
 
 static void pl_QSqlDatabase()
@@ -38,6 +52,7 @@ static void pl_processEvents()
 // clang-format off
 T_TEST_BEGIN
     MEMLEAKTEST(pl_nothing),
+    MEMLEAKTEST(pl_no_app_codec),
     MEMLEAKTEST(pl_QSqlDatabase),
     MEMLEAKTEST(pl_processEvents)
 T_TEST_END

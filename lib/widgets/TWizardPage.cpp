5f828a879fb1ad18ddd3ade1fae3f9783c7e8213
Graham Percival <gperciva@tarsnap.com>
2020-02-04 23:08:09 -0800

    TWizard, TWizardPage: add replacements for QWizard, QWizardPage
    QWizard is almost exactly what I wanted for SetupWizard (the replacement
    for SetupDialog), but a number of things are too hard-coded.  For
    example:
    - it's impossible to increase the font size of the title, and the logo
      isn't displayed unless there's a subtitle.
    - there's some corner cases in keyboard navigation, wherein a navigation
      button can retain visual focus while a widget inside a QWizardPage is
      being edited.
    After many hours of investigating workarounds and existing Qt bug
    reports (many of which were deemed "out of scope"), I decided that it
    would be easier to write my own classes.
    
    TWizard and TWizardPage keep everything easily modifiable.  They were
    written specifically for my use-case, so they diverge from QWizard and
    QWizardPage in a few important aspects:
    
    - pages must be added all at once via TWizard::addPages(), instead of
      one-by-one with QWizard::addPage().
    - it does not support the field() and registerField() mechanism.
    - it only supports a linear wizard, rather than non-linear ones.
    - it does not support subtitles.
    - it has 4 buttons; from left to right: skipButton, backButton,
      nextButton, and finishButton.  Connections are created "automatically"
      via TWizardPage::connect_ui_buttons().

---
+++ lib/widgets/TWizardPage.cpp
@@ -0,0 +1,111 @@
+#include "TWizardPage.h"
+
+WARNINGS_DISABLE
+#include <QAbstractButton>
+#include <QString>
+#include <QWidget>
+WARNINGS_ENABLE
+
+#define GET_BUTTON(BUTTON) button = findChild<QAbstractButton *>(BUTTON)
+
+TWizardPage::TWizardPage(QWidget *parent)
+    : QWidget(parent), _proceedButton(nullptr)
+{
+}
+
+void TWizardPage::initializePage()
+{
+    connect_ui_buttons();
+    checkComplete();
+}
+
+bool TWizardPage::checkComplete()
+{
+    return setProceedButton(true);
+}
+
+bool TWizardPage::setProceedButton(bool enable)
+{
+    _proceedButton->setEnabled(enable);
+    return enable;
+}
+
+QString TWizardPage::title() const
+{
+    return _title;
+}
+
+void TWizardPage::setTitle(QString const &text)
+{
+    _title = text;
+}
+
+void TWizardPage::connect_ui_buttons()
+{
+    QAbstractButton *button;
+
+    if((GET_BUTTON("skipButton")) != nullptr)
+        connect(button, &QAbstractButton::clicked, this, &TWizardPage::skip);
+    if((GET_BUTTON("backButton")) != nullptr)
+        connect(button, &QAbstractButton::clicked, this, &TWizardPage::back);
+    if((GET_BUTTON("nextButton")) != nullptr)
+    {
+        connect(button, &QAbstractButton::clicked, this, &TWizardPage::next);
+        Q_ASSERT(_proceedButton == nullptr);
+        _proceedButton = button;
+    }
+    if((GET_BUTTON("finishButton")) != nullptr)
+    {
+        connect(button, &QAbstractButton::clicked, this, &TWizardPage::finish);
+        Q_ASSERT(_proceedButton == nullptr);
+        _proceedButton = button;
+    }
+    Q_ASSERT(_proceedButton != nullptr);
+}
+
+QAbstractButton *TWizardPage::button(const WizardButton which) const
+{
+    QAbstractButton *button;
+    switch(which)
+    {
+    case SkipButton:
+        GET_BUTTON("skipButton");
+        Q_ASSERT(button != nullptr);
+        return button;
+    case BackButton:
+        GET_BUTTON("backButton");
+        Q_ASSERT(button != nullptr);
+        return button;
+    case NextButton:
+        GET_BUTTON("nextButton");
+        Q_ASSERT(button != nullptr);
+        return button;
+    case FinishButton:
+        GET_BUTTON("finishButton");
+        Q_ASSERT(button != nullptr);
+        return button;
+    }
+    // Shouldn't reach here.
+    return nullptr;
+}
+
+// These methods are here so that derived classes can override them.
+void TWizardPage::skip()
+{
+    emit skipWizard();
+}
+
+void TWizardPage::back()
+{
+    emit backPage();
+}
+
+void TWizardPage::next()
+{
+    emit nextPage();
+}
+
+void TWizardPage::finish()
+{
+    emit finishWizard();
+}
---
+++ lib/widgets/TWizardPage.h
@@ -0,0 +1,99 @@
+#ifndef TWIZARDPAGE_H
+#define TWIZARDPAGE_H
+
+#include "warnings-disable.h"
+
+WARNINGS_DISABLE
+#include <QString>
+#include <QWidget>
+WARNINGS_ENABLE
+
+/* Forward declaration. */
+class QAbstractButton;
+
+/*!
+ * \ingroup widgets
+ * \brief The TWizardPage is a QWidget which is similar to a QWizardPage,
+ * but with more control over the appearance and behaviour.
+ *
+ * The major divergence from QWizardPage is that the navigation buttons are
+ * within the UI for each page, not the main TWizard dialog.  This allows
+ * TWizardPage to decide whether or not to enable the Next or Finish button,
+ * what to do if the user clicks on it, etc.  All that the TWizard care
+ * about is whether it receives a NextPage or FinishPage signal.
+ *
+ * This avoids the whole QWizardPage::isComplete(),
+ * QWizardPage::validatePage(), and QWizardPage::completeChanged()
+ * infrastructure.  In addition, it makes keyboard navigation (and focus)
+ * easier for Qt to handle, thereby avoiding an odd bug wherein a navigation
+ * button and widget inside a QWizardPage both appearing to have keyboard
+ * focus at the same time.
+ */
+class TWizardPage : public QWidget
+{
+    Q_OBJECT
+
+    //! Page title.
+    Q_PROPERTY(QString title READ title WRITE setTitle DESIGNABLE true)
+
+public:
+    //! Constructor.
+    TWizardPage(QWidget *parent = nullptr);
+
+    //! Initialize the page when it first becomes visible.
+    virtual void initializePage();
+
+    //! Get the current page's title.
+    QString title() const;
+    //! Set the title.
+    void setTitle(QString const &text);
+
+    //! Navigation buttons.
+    enum WizardButton
+    {
+        SkipButton,
+        BackButton,
+        NextButton,
+        FinishButton
+    };
+
+    //! Get a navigation button.
+    QAbstractButton *button(const WizardButton which) const;
+
+signals:
+    //! Stop the wizard at the current point and accept the current config.
+    void skipWizard();
+    //! Move to the previous page.
+    void backPage();
+    //! Move to the next page.
+    void nextPage();
+    //! Finish the wizard.
+    void finishWizard();
+
+protected:
+    //! User activated the "skip" button.
+    virtual void skip();
+    //! User activated the "back" button.
+    virtual void back();
+    //! User activated the "next" button.
+    virtual void next();
+    //! User activated the "finish" button.
+    virtual void finish();
+
+    //! Has the user filled in all mandatory data on the page?
+    virtual bool checkComplete();
+
+    //! Enable (or not) the Next or Finish button.
+    bool setProceedButton(bool enable);
+
+private:
+    QString _title;
+
+    //! Either the "next" or "finish" button.
+    QAbstractButton *_proceedButton;
+
+    // Called in initializePage, after the UI has been set up.
+    void connect_ui_buttons();
+};
+
+#endif /* TWIZARDPAGE_H */

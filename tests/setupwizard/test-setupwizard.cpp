a9edd4a147ceaacda21178ee779646b3d2e1c181
Graham Percival <gperciva@tarsnap.com>
2020-02-05 17:07:43 -0800

    tests: more cleanup wait-time
    Just like dc33015ae6c3f70834e906577f34b0e0fd309754.
    
    Not necessary for the existing SetupDialog, but will be needed when we
    switch to SetupWizard.

--- tests/setupwizard/test-setupwizard.cpp
+++ tests/setupwizard/test-setupwizard.cpp
@@ -56,6 +56,9 @@ void TestSetupWizard::initTestCase()
 void TestSetupWizard::cleanupTestCase()
 {
     TSettings::destroy();
+
+    // Wait up to 5 seconds to delete objects scheduled with ->deleteLater()
+    QCoreApplication::processEvents(QEventLoop::AllEvents, 5000);
 }
 
 void TestSetupWizard::do_nothing()

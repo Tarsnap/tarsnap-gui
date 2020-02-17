0a33e1331c077308cd750e64401733e544579643
Graham Percival <gperciva@tarsnap.com>
2020-02-17 11:29:04 -0800

    tests/valgrind: add qtest-gui-weird
    These appear to resolve the SetupWizard leak, but I admit that I'm not
    certain what's happening.  I also expect that these are non-minimal
    examples.  However, I've spent more time than I wanted to on this, so
    I'm adding it "as-is" and moving on.
    
    FUTURE: I plan to use this file as a dumping ground for messy valgrind
    stuff, and move stuff to other directories if/when I think that I've
    reduced it to a minimal example.

--- tests/valgrind/generate.sh
+++ tests/valgrind/generate.sh
@@ -9,6 +9,7 @@ DIRS="${DIRS} minimal simple network"
 DIRS="${DIRS} qtest-minimal"
 DIRS="${DIRS} network"
 DIRS="${DIRS} qtest-gui-minimal qtest-gui-simple qtest-gui-complex"
+DIRS="${DIRS} qtest-gui-weird"
 DIRS="${DIRS} network"
 DIRS="${DIRS} gui-simple"
 
---
+++ tests/valgrind/qtest-gui-weird/qtest-gui-weird.cpp
@@ -0,0 +1,115 @@
+#include "../warnings-disable.h"
+
+WARNINGS_DISABLE
+#include <QtTest/QtTest>
+
+#include <QCheckBox>
+#include <QComboBox>
+#include <QDialog>
+#include <QFileSystemModel>
+#include <QFontMetrics>
+#include <QLabel>
+#include <QLineEdit>
+#include <QRadioButton>
+#include <QSequentialAnimationGroup>
+#include <QStackedWidget>
+#include <QTreeView>
+#include <QVBoxLayout>
+WARNINGS_ENABLE
+
+class TestQTestWeird : public QObject
+{
+    Q_OBJECT
+
+private slots:
+    void pl_nothing();
+    void pl_setvisible();
+    void pl_stackedwidget1();
+    void pl_stackedwidget2();
+};
+
+void TestQTestWeird::pl_nothing()
+{
+}
+
+void TestQTestWeird::pl_setvisible()
+{
+    QWidget *wid = new QWidget();
+    wid->setLayout(new QVBoxLayout());
+
+    QWidget *p1 = new QWidget();
+    QWidget *p2 = new QWidget();
+    wid->layout()->addWidget(p1);
+    wid->layout()->addWidget(p2);
+
+    // We only need to set this on one of them.
+    p1->setLayout(new QVBoxLayout());
+
+    wid->show();
+    p1->hide();
+    p1->show();
+
+    delete wid;
+}
+
+void TestQTestWeird::pl_stackedwidget1()
+{
+    QWidget *wid = new QWidget();
+    wid->setLayout(new QVBoxLayout());
+    QStackedWidget *p1 = new QStackedWidget();
+    QStackedWidget *p2 = new QStackedWidget();
+    wid->layout()->addWidget(p1);
+    wid->layout()->addWidget(p2);
+
+    QWidget *hideme = new QWidget();
+    hideme->setLayout(new QVBoxLayout());
+    QWidget *inner = new QWidget();
+    inner->setLayout(new QVBoxLayout());
+    inner->layout()->addWidget(new QLabel());
+    inner->layout()->addWidget(new QLineEdit());
+    hideme->layout()->addWidget(inner);
+    hideme->hide();
+
+    wid->layout()->addWidget(hideme);
+
+    wid->show();
+
+    hideme->show();
+    hideme->hide();
+    p1->hide();
+    p1->show();
+    p2->hide();
+    p2->show();
+
+    delete wid;
+}
+
+void TestQTestWeird::pl_stackedwidget2()
+{
+    QStackedWidget *wid = new QStackedWidget();
+
+    QStackedWidget *p1 = new QStackedWidget();
+    QStackedWidget *p2 = new QStackedWidget();
+    wid->addWidget(p1);
+    wid->addWidget(p2);
+
+    QWidget *hideme = new QWidget();
+    hideme->setLayout(new QVBoxLayout());
+    QWidget *inner = new QWidget();
+    inner->setLayout(new QVBoxLayout());
+    inner->layout()->addWidget(new QLabel());
+    inner->layout()->addWidget(new QLineEdit());
+    hideme->layout()->addWidget(inner);
+    hideme->hide();
+
+    p1->addWidget(hideme);
+
+    wid->show();
+    wid->setCurrentIndex(1);
+    wid->setCurrentIndex(0);
+
+    delete wid;
+}
+
+QTEST_MAIN(TestQTestWeird)
+#include "qtest-gui-weird.moc"

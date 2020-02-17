d4f081f050b5d9dc77f4ef0c095efb7177784068
Graham Percival <gperciva@tarsnap.com>
2020-02-17 10:54:24 -0800

    tests/valgrind: omit all leaks using libfontconfig
    Either that library, or Qt's use of that library, is a leaky disaster.
    In the old system, I tried to keep track of how each leak originated,
    but I've given up now.  For my purpose of "did I make a mistake writing
    new code?", I think that it's sufficient to ignore all
    libfontconfig-related leaks.

--- tests/valgrind/supp-generalize.py
+++ tests/valgrind/supp-generalize.py
@@ -42,6 +42,12 @@ def process_chunk(chunk_pre):
             chunk_post = lines[:i]
             break
 
+        # Omit anything relating to libfontconfig, because it's a nightmare
+        # (but keep one such line).
+        if line.startswith("obj:") and "libfontconfig" in line:
+            chunk_post = lines[:i + 1]
+            break
+
     # Strip any final "obj:*" lines since they're not helpful.
     while "obj:*" in chunk_post[-1]:
         chunk_post = chunk_post[:-1]

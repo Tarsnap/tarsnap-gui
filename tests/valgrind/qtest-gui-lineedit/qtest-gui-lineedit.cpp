005b8e46347a784951c996e4c54d81d5ff95448f
Graham Percival <gperciva@tarsnap.com>
2020-02-20 20:56:13 -0800

    tests/valgrind: new dir qtest-gui-lineedit
    These functions were copied from qtest-gui-complex.cpp.
    
    *NOTE:* I'm keeping the files in qtest-gui-complex as well, because on
    at least one platform, at least one of them reveals a leak [1] (?!?).
    
    [1] it's a "reachable leak" instead of a "definite leak", but still
    frustrating.  QLineEdit is definitely trying to do things too cleverly.

---
+++ tests/valgrind/qtest-gui-lineedit/qtest-gui-lineedit.cpp
@@ -0,0 +1,65 @@
+#include "../warnings-disable.h"
+
+WARNINGS_DISABLE
+#include <QtTest/QtTest>
+
+#include <QLineEdit>
+WARNINGS_ENABLE
+
+class TestQTestLineEdit : public QObject
+{
+    Q_OBJECT
+
+private slots:
+    void pl_nothing();
+    void pl_lineedit_text();
+    void pl_lineedit_text_clear();
+    void pl_lineedit_clear_show();
+    void pl_lineedit_placeholder();
+};
+
+void TestQTestLineEdit::pl_nothing()
+{
+}
+
+void TestQTestLineEdit::pl_lineedit_text()
+{
+    QLineEdit *le = new QLineEdit();
+
+    le->setClearButtonEnabled(true);
+    le->setText("Let's cause a memory leak!");
+
+    delete le;
+}
+
+void TestQTestLineEdit::pl_lineedit_text_clear()
+{
+    QLineEdit *le = new QLineEdit();
+
+    le->setClearButtonEnabled(true);
+    le->setText("Let's cause a memory leak!");
+    le->clear();
+
+    delete le;
+}
+
+void TestQTestLineEdit::pl_lineedit_clear_show()
+{
+    QLineEdit *le = new QLineEdit();
+
+    le->show();
+    le->setClearButtonEnabled(true);
+    le->setPlaceholderText("placeholder");
+    le->setText("text");
+    delete le;
+}
+
+void TestQTestLineEdit::pl_lineedit_placeholder()
+{
+    QLineEdit *le = new QLineEdit();
+    le->setPlaceholderText("this is a long piece of placeholder text");
+    delete le;
+}
+
+QTEST_MAIN(TestQTestLineEdit)
+#include "qtest-gui-lineedit.moc"

b6edaf2e1f273728aca757a3323aefc8f9751c93
Graham Percival <gperciva@tarsnap.com>
2020-02-20 20:56:10 -0800

    tests/valgrind: add gui-network, qtest-gui-network
    As far as I know, these don't find any memory leaks not already found by
    the plain network/ test on my current platform.  But it might be useful
    to have this around to eliminate possible leak causes, especially on
    other platforms.

---
+++ tests/valgrind/qtest-gui-network/qtest-gui-network.cpp
@@ -0,0 +1,70 @@
+#include "../warnings-disable.h"
+
+WARNINGS_DISABLE
+#include <QNetworkAccessManager>
+#include <QThread>
+#include <QtTest/QtTest>
+WARNINGS_ENABLE
+
+class TestQTestNetwork : public QObject
+{
+    Q_OBJECT
+
+private slots:
+    void pl_nothing();
+
+    void pl_networkaccessmanager();
+    void pl_networkaccessmanager_wait();
+    void pl_networkaccessmanager_status();
+    void pl_networkaccessmanager_repeated();
+
+private:
+    void pl_networkaccessmanager_status_not_solo();
+};
+
+void TestQTestNetwork::pl_nothing()
+{
+}
+
+void TestQTestNetwork::pl_networkaccessmanager()
+{
+    QNetworkAccessManager *nam = new QNetworkAccessManager();
+    delete nam;
+}
+
+void TestQTestNetwork::pl_networkaccessmanager_wait()
+{
+    QNetworkAccessManager *nam = new QNetworkAccessManager();
+    QThread::msleep(200);
+    delete nam;
+}
+
+void TestQTestNetwork::pl_networkaccessmanager_status_not_solo()
+{
+    QNetworkAccessManager *nam = new QNetworkAccessManager();
+
+    QThread::msleep(200);
+    nam->networkAccessible();
+    QThread::msleep(200);
+
+    delete nam;
+}
+
+void TestQTestNetwork::pl_networkaccessmanager_status()
+{
+    pl_networkaccessmanager_status_not_solo();
+}
+
+void TestQTestNetwork::pl_networkaccessmanager_repeated()
+{
+    // QNetworkAccessManger has an unpredictable interactions with dbus,
+    // causing variable memory leaks.  This is my attempt at catching them.
+    for(int i = 0; i < 10; i++)
+    {
+        pl_networkaccessmanager_status_not_solo();
+        QThread::msleep(200);
+    }
+}
+
+QTEST_MAIN(TestQTestNetwork)
+#include "qtest-gui-network.moc"

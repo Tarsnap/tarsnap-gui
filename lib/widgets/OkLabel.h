c72c5f43bc208b3527af4cb4a9da89983d900de7
Graham Percival <gperciva@tarsnap.com>
2020-02-23 13:53:53 -0800

    lib: add OkLabel

---
+++ lib/widgets/OkLabel.h
@@ -0,0 +1,47 @@
+#ifndef OKLABEL_H
+#define OKLABEL_H
+
+#include "warnings-disable.h"
+
+WARNINGS_DISABLE
+#include <QLabel>
+WARNINGS_ENABLE
+
+/*!
+ * \ingroup lib-widgets
+ * \brief The OkLabel widget is a QLabel which displays success,
+ * error, or nothing.
+ *
+ * In English, this is displayed by a green check, red cross, or blank;
+ * it would be easy to alter this for other languages.
+ */
+class OkLabel : public QLabel
+{
+    Q_OBJECT
+
+    //! Status to display.  We use int to avoid dealing with QMetaType,
+    //! but the only meaningful values are listed in \ref Status.
+    Q_PROPERTY(int status READ status WRITE setStatus DESIGNABLE true)
+
+public:
+    //! Constructor.
+    explicit OkLabel(QWidget *parent = nullptr);
+
+    //! What state should we display?
+    enum Status
+    {
+        Unset = 0,
+        Ok    = 1,
+        Error = 2
+    };
+
+    //! Get the status.
+    int status() const;
+    //! Set the status.
+    void setStatus(int ok);
+
+private:
+    int _ok;
+};
+
+#endif // OKLABEL_H

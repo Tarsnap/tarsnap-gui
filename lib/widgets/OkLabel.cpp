952f35f7c03f6e75fb5fcb556a46e997612f3dc4
Graham Percival <gperciva@tarsnap.com>
2020-02-24 10:04:27 -0800

    OkLabel: fix variable name

--- lib/widgets/OkLabel.cpp
+++ lib/widgets/OkLabel.cpp
@@ -1,6 +1,6 @@
 #include "OkLabel.h"
 
-OkLabel::OkLabel(QWidget *parent) : QLabel(parent), _ok(0)
+OkLabel::OkLabel(QWidget *parent) : QLabel(parent), _status(0)
 {
     setAlignment(Qt::AlignCenter);
     setFixedSize(15, 15);
@@ -8,14 +8,14 @@ OkLabel::OkLabel(QWidget *parent) : QLabel(parent), _ok(0)
 
 int OkLabel::status() const
 {
-    return _ok;
+    return _status;
 }
 
-void OkLabel::setStatus(int ok)
+void OkLabel::setStatus(int status)
 {
-    _ok = ok;
+    _status = status;
 
-    switch(ok)
+    switch(status)
     {
     case Unset:
         setText("");

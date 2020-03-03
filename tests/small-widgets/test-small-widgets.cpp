120c501cb3cbf668f97b86cba482cf9bb7fed8f7
Graham Percival <gperciva@tarsnap.com>
2020-03-02 18:07:16 -0800

    tests/small-widgets: check ConfirmationDialog countdown

--- tests/small-widgets/test-small-widgets.cpp
+++ tests/small-widgets/test-small-widgets.cpp
@@ -163,6 +163,32 @@ void TestSmallWidgets::confirmationDialog()
     QVERIFY((sig_confirmed.count() == 0) && (sig_cancel.count() == 1));
     sig_cancel.clear();
 
+    // Launch the window, enter 'confirm', then cancel the countdown.
+    cd->start("text title", "type 'confirm'", "confirm", 1, "countdown title",
+              "seconds left %1", "confirmed");
+    VISUAL_WAIT;
+    cd->_inputDialog.setTextValue("confirm");
+    VISUAL_WAIT;
+    cd->_inputDialog.accept();
+    VISUAL_WAIT;
+    cd->_countdownBox.reject();
+    QVERIFY((sig_confirmed.count() == 0) && (sig_cancel.count() == 1));
+    sig_cancel.clear();
+
+    // Launch the window, enter 'confirm', then wait for the countdown.
+    cd->start("text title", "type 'confirm'", "confirm", 1, "countdown title",
+              "seconds left %1", "confirmed");
+    QVERIFY(cd->_inputDialog.textValue() == "");
+    VISUAL_WAIT;
+    cd->_inputDialog.setTextValue("confirm");
+    VISUAL_WAIT;
+    cd->_inputDialog.accept();
+    VISUAL_WAIT;
+    while(sig_confirmed.count() == 0)
+        QCoreApplication::processEvents(QEventLoop::AllEvents, 100);
+    QVERIFY((sig_confirmed.count() == 1) && (sig_cancel.count() == 0));
+    sig_confirmed.clear();
+
     delete cd;
 }
 

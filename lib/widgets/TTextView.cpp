c438b2942c816ee35cf2f97720df1c01754bacb6
Graham Percival <gperciva@tarsnap.com>
2020-03-04 09:51:58 -0800

    TTextView: add

---
+++ lib/widgets/TTextView.cpp
@@ -0,0 +1,42 @@
+#include "TTextView.h"
+
+#include <QApplication>
+
+TTextView::TTextView(QWidget *parent)
+    : QPlainTextEdit(parent), _cursor(document())
+{
+    setReadOnly(true);
+
+    // Store these values for easier re-use.
+    _baseColor = qApp->palette().base().color();
+    _altColor  = qApp->palette().alternateBase().color();
+}
+
+void TTextView::appendLog(const LogEntry &log)
+{
+    appendAlternatingText(
+        QString("[%1] %2")
+            .arg(log.timestamp.toString(Qt::DefaultLocaleShortDate))
+            .arg(log.message));
+}
+
+void TTextView::appendAlternatingText(const QString &text)
+{
+    // Start a new block.
+    if(!document()->isEmpty())
+        _cursor.insertBlock();
+
+    // Alternate the background color.
+    QTextBlockFormat fmt;
+    if(_cursor.blockFormat().background().color() == _baseColor)
+        fmt.setBackground(QBrush(_altColor));
+    else
+        fmt.setBackground(QBrush(_baseColor));
+    _cursor.mergeBlockFormat(fmt);
+
+    // Append the text.
+    _cursor.insertText(text);
+
+    // Scroll to display the recently-added text.
+    ensureCursorVisible();
+}
---
+++ lib/widgets/TTextView.h
@@ -0,0 +1,40 @@
+#ifndef TTEXTVIEW_H
+#define TTEXTVIEW_H
+
+#include "warnings-disable.h"
+
+WARNINGS_DISABLE
+#include <QColor>
+#include <QPlainTextEdit>
+#include <QTextCursor>
+WARNINGS_ENABLE
+
+#include "LogEntry.h"
+
+/*!
+ * \ingroup lib-widgets
+ * \brief The TTextView widget is a QPlainTextEdit which displays log messages,
+ * with alternating background colors.
+ */
+class TTextView : public QPlainTextEdit
+{
+    Q_OBJECT
+
+public:
+    //! Constructor.
+    TTextView(QWidget *parent = nullptr);
+
+    //! Append a log message to the display.
+    void appendLog(const LogEntry &log);
+
+private:
+    // Convenience variables (to avoid regenerating them all the time).
+    QTextCursor _cursor;
+    QColor      _baseColor;
+    QColor      _altColor;
+
+    // Append text, with alternating background colors.
+    void appendAlternatingText(const QString &text);
+};
+
+#endif // !TTEXTVIEW_H

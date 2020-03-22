3a5e8fd9897441ef3ce8e6c14e206b9d28fec4b7
Graham Percival <gperciva@tarsnap.com>
2020-03-22 11:09:33 -0700

    tests/taskmanager: add stopping running tests

--- tests/taskmanager/test-taskmanager.cpp
+++ tests/taskmanager/test-taskmanager.cpp
@@ -28,6 +28,8 @@ private slots:
     void sleep_cancel();
     void sleep_task();
     void sleep_task_cancel();
+    void sleep_task_cancel_running_parallel();
+    void sleep_task_cancel_running_series();
     void tarsnapVersion_fake();
     void registerMachine_fake();
 };
@@ -237,6 +239,74 @@ void TestTaskManager::sleep_task_cancel()
     QVERIFY(sig_message.takeFirst().at(0).toString() == "Finished sleep task.");
 
     // task is deleted by the task manager
+    delete manager;
+}
+
+void TestTaskManager::sleep_task_cancel_running_parallel()
+{
+    // Set up the manager.
+    TaskManager *manager = new TaskManager();
+    QSignalSpy   sig_message(manager, SIGNAL(message(QString, QString)));
+    int          start_messages = 0;
+    int          end_messages   = 0;
+
+    // Set up two tasks running at the same time.
+    manager->sleepSeconds(5, false);
+    start_messages += 1;
+    end_messages += 1;
+    WAIT_UNTIL(sig_message.count() >= start_messages);
+
+    manager->sleepSeconds(5, false);
+    start_messages += 1;
+    end_messages += 1;
+    WAIT_UNTIL(sig_message.count() >= start_messages);
+
+    // Cancel them.
+    manager->stopTasks(false, true, false);
+    end_messages += 1;
+    WAIT_UNTIL(sig_message.count() >= (start_messages + end_messages));
+
+    // Tasks are deleted by the task manager.
+    delete manager;
+}
+
+void TestTaskManager::sleep_task_cancel_running_series()
+{
+    // Set up the manager.
+    TaskManager *manager = new TaskManager();
+    QSignalSpy   sig_message(manager, SIGNAL(message(QString, QString)));
+    QVariant     msg;
+
+    // Set up two tasks running one after the other.
+    manager->sleepSeconds(5, true);
+    manager->sleepSeconds(5, true);
+
+    WAIT_UNTIL(sig_message.count() >= 1);
+    msg = sig_message.takeFirst().at(0);
+    QVERIFY(msg.toString() == "Started sleep task.");
+    QVERIFY(sig_message.count() == 0);
+
+    // Cancel one of them.
+    manager->stopTasks(false, true, false);
+    WAIT_UNTIL(sig_message.count() >= 3);
+    msg = sig_message.takeFirst().at(0);
+    QVERIFY(msg.toString() == "Stopped running tasks.");
+    msg = sig_message.takeFirst().at(0);
+    QVERIFY(msg.toString() == "Finished sleep task.");
+    msg = sig_message.takeFirst().at(0);
+    QVERIFY(msg.toString() == "Started sleep task.");
+    QVERIFY(sig_message.count() == 0);
+
+    // Cancel the other one.
+    manager->stopTasks(false, true, false);
+    WAIT_UNTIL(sig_message.count() >= 2);
+    msg = sig_message.takeFirst().at(0);
+    QVERIFY(msg.toString() == "Stopped running tasks.");
+    msg = sig_message.takeFirst().at(0);
+    QVERIFY(msg.toString() == "Finished sleep task.");
+    QVERIFY(sig_message.count() == 0);
+
+    // Tasks are deleted by the task manager.
     delete manager;
 }
 

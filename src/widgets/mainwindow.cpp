Tarsnap/tarsnap-gui

commit ce643b5346fa0ee3bb5035a16617846e4d78c2fa

MainWindow: remove getTaskInfo() signal

src/widgets/mainwindow.cpp
@@ -97,8 +97,6 @@ MainWindow::MainWindow(QWidget *parent)
     connect(_ui->statusBarLabel, &ElidedClickableLabel::clicked,
             _ui->actionShowJournal, &QAction::toggle);
     addAction(_ui->actionStopTasks);
-    connect(_ui->actionStopTasks, &QAction::triggered, this,
-            &MainWindow::getTaskInfo);
     connect(_ui->busyLabel, &TBusyLabel::clicked, _ui->actionStopTasks,
             &QAction::trigger);
     // --

src/widgets/mainwindow.h
@@ -119,9 +119,6 @@ signals:
     //! Begin tarsnap --version
     //! \anchor tarsnapVersionRequested
     void tarsnapVersionRequested(QString tarsnapPath);
-    //! Query whether there are any running tasks; will trigger a taskInfo
-    //! signal which is received by \ref closeWithTaskInfo.
-    void getTaskInfo();
     //! Clear all Journal entries.
     void clearJournal();
     //! Begin tarsnap-keymgmt --print-key-id \<key_filename\>

src/app-gui.cpp
@@ -214,8 +214,6 @@ void AppGui::showMainWindow()
             &MainWindow::jobList, QUEUED);
     connect(_mainWindow, &MainWindow::deleteJob, &_taskManager,
             &TaskManager::deleteJob, QUEUED);
-    connect(_mainWindow, &MainWindow::getTaskInfo, &_taskManager,
-            &TaskManager::getTaskInfo, QUEUED);
     connect(&_taskManager, &TaskManager::taskInfo, _mainWindow,
             &MainWindow::closeWithTaskInfo, QUEUED);
     connect(_mainWindow, &MainWindow::jobAdded, &_taskManager,

commit 42767901e31645ea012c5b876886f998fa2b9926

MainWindow: fix broken "stop tasks without quitting"
I broke this in ce643b5346fa0ee3bb5035a16617846e4d78c2fa.

I'm not wild about the function name, but I think it's more important to
get this working again (and tested!), rather than fussing about the name.
I can refactor it later, if/when I come up with something better.

src/widgets/mainwindow.cpp
@@ -97,6 +97,8 @@ MainWindow::MainWindow(QWidget *parent)
     connect(_ui->statusBarLabel, &ElidedClickableLabel::clicked,
             _ui->actionShowJournal, &QAction::toggle);
     addAction(_ui->actionStopTasks);
+    connect(_ui->actionStopTasks, &QAction::triggered, this,
+            &MainWindow::nonquitStopTasks);
     connect(_ui->busyLabel, &TBusyLabel::clicked, _ui->actionStopTasks,
             &QAction::trigger);
     // --
@@ -303,6 +305,11 @@ void MainWindow::closeEvent(QCloseEvent *event)
         closeWithTaskInfo(_backupTaskRunning, _runningTasks, _queuedTasks);
         event->ignore();
     }
+}
+
+void MainWindow::nonquitStopTasks()
+{
+    closeWithTaskInfo(_backupTaskRunning, _runningTasks, _queuedTasks);
 }
 
 void MainWindow::closeWithTaskInfo(bool backupTaskRunning, int runningTasks,

src/widgets/mainwindow.h
@@ -169,6 +169,8 @@ private slots:
     void displayJobDetails(JobPtr job);
     void createNewJob(QList<QUrl> urls, QString name);
 
+    void nonquitStopTasks();
+
 private:
     Ui::MainWindow *_ui;
     int             _minWidth;
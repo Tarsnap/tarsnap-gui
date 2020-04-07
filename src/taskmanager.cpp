Tarsnap/tarsnap-gui

commit 3a8df8aced48c3cc413f3fb448bf4afa07d47fd8

TaskManager: parse stats later in backupTaskFinished()
This will minimize the diff when we switch to BackendData.

I'm fairly confident that this is a harmless change. but I'm keeping it
as a separate commit in case it turns out to be problematic.

src/taskmanager.cpp
@@ -394,7 +394,6 @@ void TaskManager::backupTaskFinished(QVariant data, int exitCode,
     archive->setTimestamp(
         QDateTime::fromTime_t(backupTaskData->timestamp().toTime_t()));
     archive->setJobRef(backupTaskData->jobRef());
-    parseArchiveStats(stdErr, true, archive);
     archive->save();
     backupTaskData->setArchive(archive);
     backupTaskData->setStatus(TaskStatus::Completed);
@@ -404,6 +403,7 @@ void TaskManager::backupTaskFinished(QVariant data, int exitCode,
         if(job->objectKey() == archive->jobRef())
             emit job->loadArchives();
     }
+    parseArchiveStats(stdErr, true, archive);
     emit archiveAdded(archive);
     parseGlobalStats(stdErr);
 }

commit f4f7c68be3ea8b45f65927d69ce5c9b87ade1ca9

TaskManager: fix order of stats & notify
I broke this in 3a8df8aced48c3cc413f3fb448bf4afa07d47fd8, when I moved
parseArchiveStats() below backupTaskData->setStatus().

(Other potentially-relevant commits are
  - move backupTaskData->setStatus() back to TaskManager
    b1390d43d67b3908dec1dd10df95377c28b7f1fd
  - call notifyBackupTaskUpdate() directly instead of setStatus()
    0a5ca0b9a2435c646bba74e4679c2dfad843d3ab
but neither of those are the original problem.)

src/taskmanager.cpp
@@ -353,8 +353,10 @@ void TaskManager::backupTaskFinished(QVariant data, int exitCode,
     }
 
     ArchivePtr archive = _bd->newArchive(backupTaskData, truncated);
-    notifyBackupTaskUpdate(backupTaskData, TaskStatus::Completed);
     parseArchiveStats(stdErr, true, archive);
+    // This needs the archive stats.
+    notifyBackupTaskUpdate(backupTaskData, TaskStatus::Completed);
+
     emit archiveAdded(archive);
 
     parseGlobalStats(stdErr);